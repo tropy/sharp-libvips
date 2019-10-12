03874acf5ce1f0d91d803dbe508ad2d0e316eecf
Sylvester Keil <sylvester@keil.or.at>
2019-10-12 14:41:10 +0200

    Update test scripts

--- test/archlinux.sh
+++ test/archlinux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Install Node.js on Archlinux
-pacman -Sy --noconfirm gcc make python2 nodejs npm | cat
+pacman -Sy --noconfirm gcc make python2 nodejs npm orc fftw | cat
 ln -s /usr/bin/python2 /usr/bin/python
--- test/centos.sh
+++ test/centos.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 curl -sL https://rpm.nodesource.com/setup_6.x | bash -
-yum install -y gcc-c++ make nodejs
+yum install -y gcc-c++ make nodejs fftw orc
