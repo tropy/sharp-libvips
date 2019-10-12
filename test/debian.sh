d5ba2af03f33f44e50d6505a975f9e46ac346f35
Sylvester Keil <sylvester@keil.or.at>
2019-10-12 14:45:18 +0200

    Fix liborc package name

--- test/debian.sh
+++ test/debian.sh
@@ -3,4 +3,4 @@
 apt-get update
 apt-get install -y build-essential python pkg-config curl
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install -y nodejs libfftw3-bin liborc-0.4
+apt-get install -y nodejs libfftw3-bin liborc-0.4-0
